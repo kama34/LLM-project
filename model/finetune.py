293a055a32e79161957c3a811dbd5eb4522bd159
kama34 <62620760+kama34@users.noreply.github.com>
2024-07-14 01:58:26 +0300

    Update finetune.py

--- model/finetune.py
+++ model/finetune.py
@@ -42,8 +42,8 @@ ds = Dataset.from_pandas(df)
 print(ds)
 
 # Setup dataset
-MODEL_NAME = "meta-llama/Llama-3-70b"
-# MODEL_NAME = "meta-llama/Meta-Llama-3-70B-Instruct"
+# MODEL_NAME = "meta-llama/Llama-3-70b"
+MODEL_NAME = "meta-llama/Meta-Llama-3-70B-Instruct"
 
 tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, trust_remote_code=True)
 print(tokenizer.pad_token, tokenizer.eos_token)
