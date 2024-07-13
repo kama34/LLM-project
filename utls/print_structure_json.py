ee3f13882cd50030aebbf421c4f8c5429f69ed06
kama34 <62620760+kama34@users.noreply.github.com>
2024-07-13 18:25:32 +0300

    Update print_structure_json.py

--- utls/print_structure_json.py
+++ utls/print_structure_json.py
@@ -9,6 +9,15 @@ import sys
 
 def print_structure(data, indent=0):
     for key, value in data.items():
+        print('  ' * indent + str(key))
+        if isinstance(value, dict):
+            print_structure(value, indent + 1)
+        elif isinstance(value, list) and len(value) > 0 and isinstance(value[0], dict):
+            print_structure(value[0], indent + 1)
+
+
+def print_example(data, indent=0):
+    for key, value in data.items():
         if isinstance(value, dict):
             print('  ' * indent + str(key) + ': {')
             print_structure(value, indent + 1)
@@ -24,7 +33,10 @@ def print_structure(data, indent=0):
 def main(json_file_path):
     with open(json_file_path, 'r') as f:
         data = json.load(f)
+    print("\nStructure:")
     print_structure(data)
+    print("\nExample:")
+    print_example(data)
 
 
 if __name__ == "__main__":
