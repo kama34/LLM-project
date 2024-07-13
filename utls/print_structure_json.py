f10e59ea2e1b9cdd4efe8bba543d113b6cc1cbf1
kama34 <62620760+kama34@users.noreply.github.com>
2024-07-13 18:41:59 +0300

    Update print_structure_json.py

--- utls/print_structure_json.py
+++ utls/print_structure_json.py
@@ -1,33 +1,36 @@
-"""
-File which read and print structure json file
-For launch: python3 print_structure.py your_file.json
-"""
-
 import json
 import sys
 
 
 def print_example(data, indent=0):
-    for key, value in data.items():
-        if isinstance(value, dict):
-            print('  ' * indent + str(key) + ': {')
-            print_example(value, indent + 1)
-            print('  ' * indent + '}')
-        elif isinstance(value, list) and len(value) > 0 and isinstance(value[0], dict):
-            print('  ' * indent + str(key) + ': [')
-            print_example(value[0], indent + 1)
-            print('  ' * indent + ']')
-        else:
-            print('  ' * indent + str(key) + ': ' + str(value))
+    if isinstance(data, dict):
+        for key, value in data.items():
+            if isinstance(value, dict):
+                print('  ' * indent + str(key) + ': {')
+                print_example(value, indent + 1)
+                print('  ' * indent + '}')
+            elif isinstance(value, list) and len(value) > 0 and isinstance(value[0], dict):
+                print('  ' * indent + str(key) + ': [')
+                print_example(value[0], indent + 1)
+                print('  ' * indent + ']')
+            else:
+                print('  ' * indent + str(key) + ': ' + str(value))
+    elif isinstance(data, list):
+        for item in data:
+            print_example(item, indent)
 
 
 def print_structure(data, indent=0):
-    for key, value in data.items():
-        print('  ' * indent + str(key))
-        if isinstance(value, dict):
-            print_structure(value, indent + 1)
-        elif isinstance(value, list) and len(value) > 0 and isinstance(value[0], dict):
-            print_structure(value[0], indent + 1)
+    if isinstance(data, dict):
+        for key, value in data.items():
+            print('  ' * indent + str(key))
+            if isinstance(value, dict):
+                print_structure(value, indent + 1)
+            elif isinstance(value, list) and len(value) > 0 and isinstance(value[0], dict):
+                print_structure(value[0], indent + 1)
+    elif isinstance(data, list):
+        for item in data:
+            print_structure(item, indent)
 
 
 def main(json_file_path):
