b473458723400d3a7abf6d525c3fb3cbcaede69e
kama34 <62620760+kama34@users.noreply.github.com>
2024-07-13 18:32:19 +0300

    Update print_structure_json.py

--- utls/print_structure_json.py
+++ utls/print_structure_json.py
@@ -7,24 +7,36 @@ import json
 import sys
 
 
-def print_structure(data, indent=0):
+def print_example(data, indent=0):
     for key, value in data.items():
         if isinstance(value, dict):
             print('  ' * indent + str(key) + ': {')
-            print_structure(value, indent + 1)
+            print_example(value, indent + 1)
             print('  ' * indent + '}')
         elif isinstance(value, list) and len(value) > 0 and isinstance(value[0], dict):
             print('  ' * indent + str(key) + ': [')
-            print_structure(value[0], indent + 1)
+            print_example(value[0], indent + 1)
             print('  ' * indent + ']')
         else:
             print('  ' * indent + str(key) + ': ' + str(value))
 
 
+def print_structure(data, indent=0):
+    for key, value in data.items():
+        print('  ' * indent + str(key))
+        if isinstance(value, dict):
+            print_structure(value, indent + 1)
+        elif isinstance(value, list) and len(value) > 0 and isinstance(value[0], dict):
+            print_structure(value[0], indent + 1)
+
+
 def main(json_file_path):
     with open(json_file_path, 'r') as f:
         data = json.load(f)
+    print("\nStructure JSON:")
     print_structure(data)
+    print("\nExample JSON:")
+    print_example(data)
 
 
 if __name__ == "__main__":
